552dac4c39f986934d5d82ec6828a564ad7f33f8
noxa6 <noxa.hy@gmail.com>
2021-09-05 03:52:30 -0500

    :flags: creating project :flags:

---
+++ src/main/java/com/hydrogen/avuy/AvuyApplication.java
@@ -0,0 +1,13 @@
+package com.hydrogen.avuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hydrogen/avuy/AvuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydrogen.avuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
